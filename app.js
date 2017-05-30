7230bf889984cf52dd6719c08f393943df3abcd5
JH456 <james_harris@ultimatesoftware.com>
2017-05-30 11:43:06 -0400

    got it to hello world

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
 var cors = require('cors');
+var domainScannerRouter = require('./domainScanner/domainScannerRouter');
 
 app.use(cors());
+
+app.use('/scan', domainScannerRouter);
 
 module.exports = app;
