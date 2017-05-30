a1b9ae6551903678cc885a5f903997c681502485
JH456 <james_harris@ultimatesoftware.com>
2017-05-30 15:29:54 -0400

    framed out imageProcessor a little more

--- imageProcessor/imageProcessor.js
+++ imageProcessor/imageProcessor.js
@@ -1,55 +1,62 @@
 var Clarifai = require('clarifai');
 
-var processSingleImage = function(url) {
-    this.url = url;
-    var modelID = "c0c0ac362b03416da06ab3fa36fb58e3"
-    var app = new Clarifai.App(
-        'nDIeP1Baph5TsKp62QVn-VqXtm3prGOfENsgBMA5',
-        'AOneD_evVC6sVxpnykAVk6r1L_E7mstM10NBeIKa'
-    );
+var imageProcessor = function() {
 
-    var imageStats = {
-        totalPpl: 0,
-        numMale: 0,
-        numFemale: 0,
-        malePct: 0,
-        femalePct: 0,
+    var statistics = {}
+
+    var processSingleImage = function(url) {
+        this.url = url;
+        var modelID = "c0c0ac362b03416da06ab3fa36fb58e3"
+        var app = new Clarifai.App(
+            'nDIeP1Baph5TsKp62QVn-VqXtm3prGOfENsgBMA5',
+            'AOneD_evVC6sVxpnykAVk6r1L_E7mstM10NBeIKa'
+        );
+
+        var imageStats = {
+            totalPpl: 0,
+            numMale: 0,
+            numFemale: 0,
+            malePct: 0,
+            femalePct: 0,
+        }
+
+        var recordGenderData = function (person){
+            var genderData = person.data.face.gender_appearance;
+            return genderData;
+        };
+
+        var determineGender = function (genderData) {
+            var gender = genderData.concepts[0].name;
+            if (gender === "feminine"){
+                imageStats.numFemale += 1;
+            } else {
+                imageStats.numMale += 1;
+            }
+        };
+
+        var findPcts = function () {
+            imageStats.malePct = imageStats.numMale / imageStats.totalPpl * 100;
+            imageStats.femalePct = imageStats.numFemale / imageStats.totalPpl * 100;
+        };
+
+        return app.models.predict(modelID, url).then(
+            function(response) {
+                let people = response.outputs[0].data.regions;
+                imageStats.totalPpl = people.length;
+                people.map(person => {
+                    let genderData = recordGenderData(person);
+                    determineGender(genderData);
+                })
+                findPcts();
+                return imageStats;
+            },
+
+            function(err) {
+                // TODO add some logging in here
+                console.error(err);
+            }
+        );
     }
 
-    var recordGenderData = function (person){
-        var genderData = person.data.face.gender_appearance;
-        return genderData;
-    };
-
-    var determineGender = function (genderData) {
-        var gender = genderData.concepts[0].name;
-        if (gender === "feminine"){
-            imageStats.numFemale += 1;
-        } else {
-            imageStats.numMale += 1;
-        }
-    };
-
-    var findPcts = function () {
-        imageStats.malePct = imageStats.numMale / imageStats.totalPpl * 100;
-        imageStats.femalePct = imageStats.numFemale / imageStats.totalPpl * 100;
-    };
-
-    app.models.predict(modelID, url).then(
-        function(response) {
-            let people = response.outputs[0].data.regions;
-            imageStats.totalPpl = people.length;
-            people.map(person => {
-                let genderData = recordGenderData(person);
-                determineGender(genderData);
-            })
-            findPcts();
-            return imageStats;
-        },
-
-        function(err) {
-            // TODO add some logging in here
-            console.error(err);
-        }
-    );
-}
+    return {};
+};
